09a56ed5281b498be8e18b1d25e89040d8a530b6
Rebecca <135012814+rebeccaorso@users.noreply.github.com>
2023-06-16 13:07:09 +0200

    Add files via upload

---
+++ Java NB/Esercizio15Giugno/src/esercizio15giugno/Esercizio15Giugno.java
@@ -0,0 +1,80 @@
+package esercizio15giugno;
+
+import java.util.Random;
+
+public class Esercizio15Giugno {
+
+    
+    public static void main(String[] args) {
+        
+        Random r = new Random();  
+        
+
+        
+        int casuale = r.nextInt(1,21);
+        System.out.println("Il numero generato da 1 a 20 e' " + casuale);
+        
+       
+        int[] arrCasuali = new int[10]; 
+        for (int i = 1; i < arrCasuali.length; i++) {
+            arrCasuali[i] = r.nextInt(10);
+        }
+        
+        for(int valori: arrCasuali){
+        System.out.println(valori);
+        }
+        
+        if()
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+    }
+
+}
