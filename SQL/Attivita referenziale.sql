a89f6fc4feb57803dc7d48512a31decbdf5e6128
Rebecca <135012814+rebeccaorso@users.noreply.github.com>
2023-06-21 12:43:21 +0200

    Add files via upload

--- SQL/Attivita referenziale.sql
+++ SQL/Attivita referenziale.sql
@@ -15,3 +15,31 @@ select * from editore;
 insert into libro(titolo, prezzo, editore_id)values('titolo',20.00,50);
 delete from editore where id = 1;
 select * from libro;
+
+show create table libro;
+
+alter table libro
+add constraint fk_libro_editore
+foreign key(editore_id) references editore(id)
+on delete no action
+on update no action;
+
+alter table autore_libro
+add constraint fk_al_libro
+foreign key(libro_id) references libro(id)
+on delete no action
+on update cascade,
+add constraint fk_al_autore
+foreign key(autore_id) references autore(id)
+on delete no action
+on update cascade;
+
+delete from autore_libro;
+delete from libro;
+delete from editore;
+delete from autore;
+
+
+
+
+
---
+++ SQL/check.sql
@@ -0,0 +1,16 @@
+show create table libro;
+
+alter table libro
+add constraint ck_prezzo
+check (prezzo > 0),
+add constraint ck_pagine
+check (pagine > 0); 
+
+
+desc libro;
+show create table libro;
+
+insert into libro(titolo, prezzo, pagine, editore_id)
+values('test',0,10,1);
+
+alter table libro drop check ck_prezzo;
